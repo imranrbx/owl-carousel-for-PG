4b72b4938193ef1fde3dfbe8666d8713895a0c1c
Imran Qasim <imran_qasim@hotmail.com>
2016-08-07 23:32:54 +0500

    First Commit

---
+++ owl-carousel.js
@@ -0,0 +1,124 @@
+$(function() {
+
+    //Wait for Pinegrow to wake-up
+    $('body').one('pinegrow-ready', function(e, pinegrow) {
+
+        //Create new Pinegrow framework object
+        var f = new PgFramework('js.owl', 'OWL Carousel and Slider');
+
+        //This will prevent activating multiple versions of 960 grid framework, provided that other versions set the same type
+        f.type = "js.owl";
+        f.allow_single_type = true;
+        //Tell Pinegrow about the framework and loads necessary plugins
+
+        //Add resources that will be copied to components/<plugin> folder and included in the page with Pinegrow Resource manager when the plugin is first activated on a page or when Resources command is used in "Manage libraries and plugins".
+        pinegrow.addFramework(f);  
+        //That's it!!! Simple, right? A lot of magic stuff could be done here, check Bootstrap framework for inspiration.
+        
+
+        //Owl Slider
+        //==================
+        var slider = new PgComponentType('owl-carousel-slider', 'OWL Carousel and Slider');
+
+        //How can we identify DOM elements that are sliders?
+        slider.selector = ".owl_slider";
+
+        //Html code for slider, that will be inserted into the page
+        slider.code = function() {
+            var id = pinegrow.getUniqueId('owl_slider');
+            var img1 = pinegrow.getPlaceholderImage();
+            var img2 = pinegrow.getPlaceholderImage();
+            var img3 = pinegrow.getPlaceholderImage();
+            var img4 = pinegrow.getPlaceholderImage();
+
+            return '<div id="' + id + '" class="owl-carousel">\
+                    <div class="item" >\
+                        <img src="' + img1 + '">\
+                    </div>\
+                    <div class="item" >\
+                        <img src="' + img2 + '">\
+                    </div>\
+                    <div class="item" >\
+                        <img src="' + img3 + '">\
+                    </div>\
+                     <div class="item" >\
+                        <img src="' + img4 + '">\
+                    </div>\
+                 </div>';
+             };
+
+        slider.on_inserted = function($el, page) {
+            var $body = $(page.getBody());
+            var id = $el.attr('id');
+            if(id) {
+                var ini_str = 'jQuery(function($){\n $("#' + id + '").owlCarousel();\n });';
+                pinegrow.addScriptToPage(page, ini_str);
+                pinegrow.showNotice('<p>Revolution slider initialization Javascript was appended to the end of the page:</p><pre>' + escapeHtmlCode(ini_str) + '</pre><p>If you change the #id of the slider element you\'ll need to update the selector in this code. You also need to <b>include Revolution slider Javascript</b> to the page.</p>', 'Revolution Slider inserted', 'revolution-on-inserted');
+            }
+        }
+     //Set empty_placeholder to true so that empty container gets min-height set. Otherwise empty container would be invisible. This lets us see it and use it as drop target. Placeholder style gets removed as soon as we add something to this element.
+        slider.empty_placeholder = false;
+
+        //Highlight element in the tree to show it is important and has special features
+        slider.tags = 'major';
+
+        //Add it to our framework
+        f.addComponentType(slider);
+
+        //Slider
+        //==================
+        var slide = new PgComponentType('Owl-Slide', 'Slide');
+
+        //How can we identify DOM elements that are sliders?
+        slide.selector = function($el) {
+            return $el.is('div') && $el.parent().parent().is('.owl_slider');
+        };
+
+        slide.priority = 100;
+
+        //Html code for slider, that will be inserted into the page
+        slide.code = function() {
+            var img1 = pinegrow.getPlaceholderImage();
+
+            return '<div class="item">\
+                        <img src="' + img1 + '">\
+                    </div>';
+        };
+
+        //Highlight element in the tree to show it is important and has special features
+        slide.tags = 'major';
+
+        //Add it to our framework
+        f.addComponentType(slide);
+
+        var r = new PgComponentTypeResource(f.getResourceFile('./assets/owl.carousel.min.js')); //relative to plugin js file
+        r.relative_url = 'js/owl.carousel.min.js'; //what should the relative url be when resource is used on the page
+        r.source = crsaMakeFileFromUrl(r.url);
+        r.footer = true; //Recommended for JS files.
+        f.resources.add(r);
+
+        r = new PgComponentTypeResource(f.getResourceFile('./assets/owl.carousel.css'));
+        r.relative_url = 'css/owl.carousel.css';
+        r.source = crsaMakeFileFromUrl(r.url);
+        r.footer = false;
+        f.resources.add(r);
+
+          r = new PgComponentTypeResource(f.getResourceFile('./assets/owl.theme.css'));
+        r.relative_url = 'css/owl.theme.css';
+        r.source = crsaMakeFileFromUrl(r.url);
+        r.footer = false;
+        f.resources.add(r);
+
+        r = new PgComponentTypeResource(f.getResourceFile('./assets/owl.transitions.css'));
+        r.relative_url = 'css/owl.transitions.css';
+        r.source = crsaMakeFileFromUrl(r.url);
+        r.footer = false;
+        f.resources.add(r);
+
+        //Now, lets define sections and elements shown in LIB tab
+        var section = new PgFrameworkLibSection('owl-elements', 'Owl Slider');
+        //Pass components in array
+        section.setComponentTypes([slider, slide]);
+        f.addLibSection(section);
+    });
+});
