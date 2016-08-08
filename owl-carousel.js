1f934310e5bcaaa2f195be8cee7bfad5e668463c
Imran Qasim <imran_qasim@hotmail.com>
2016-08-08 05:38:06 +0500

    Added few options in ACT tab for Carousel.

--- owl-carousel.js
+++ owl-carousel.js
@@ -10,12 +10,20 @@ $(function() {
         f.type = "js.owl";
         f.allow_single_type = true;
         //Tell Pinegrow about the framework and loads necessary plugins
-
+         f.detect = function(pgPage) {
+            return pgPage.hasScript(/(^|\/)jquery.*\.js/i);
+        }
         //Add resources that will be copied to components/<plugin> folder and included in the page with Pinegrow Resource manager when the plugin is first activated on a page or when Resources command is used in "Manage libraries and plugins".
         pinegrow.addFramework(f);  
         //That's it!!! Simple, right? A lot of magic stuff could be done here, check Bootstrap framework for inspiration.
         
+        var directives_actions = []; // Will be shown in ACT tab
 
+        //Tell Pinegrow about the framework
+        pinegrow.addFramework(f);
+
+        //Add properties common to all components of this framework
+       
         //Owl Slider
         //==================
         var slider = new PgComponentType('owl-carousel-slider', 'OWL Carousel and Slider');
@@ -51,7 +59,12 @@ $(function() {
             var $body = $(page.getBody());
             var id = $el.attr('id');
             if(id) {
-                var ini_str = 'jQuery(function($){\n $("#' + id + '").owlCarousel();\n });';
+                var ini_str = 'jQuery(function($){\
+                               \n var $rows = $("div#'+id+'").attr("data-rows");\
+                               \n var $navigation = Boolean($("div#'+id+'").attr("data-navigation"));\
+                               \n var $singleItem = Boolean($("div#'+id+'").attr("data-single"));\
+                               \n if (typeof($rows) == "undefined"){$rows="4";}\
+                               \n $("div#' + id + '").owlCarousel({items:$rows,navigation:$navigation,singleItem:$singleItem);\n });';
                 pinegrow.addScriptToPage(page, ini_str);
                 pinegrow.showNotice('<p>Revolution slider initialization Javascript was appended to the end of the page:</p><pre>' + escapeHtmlCode(ini_str) + '</pre><p>If you change the #id of the slider element you\'ll need to update the selector in this code. You also need to <b>include Revolution slider Javascript</b> to the page.</p>', 'Revolution Slider inserted', 'revolution-on-inserted');
             }
@@ -90,7 +103,6 @@ $(function() {
 
         //Add it to our framework
         f.addComponentType(slide);
-
         var r = new PgComponentTypeResource(f.getResourceFile('./assets/owl.carousel.min.js')); //relative to plugin js file
         r.relative_url = 'js/owl.carousel.min.js'; //what should the relative url be when resource is used on the page
         r.source = crsaMakeFileFromUrl(r.url);
@@ -115,6 +127,90 @@ $(function() {
         r.footer = false;
         f.resources.add(r);
 
+         //Properties are arranged in sections
+        var application_action = new PgComponentType('owl-app-rows', 'Number of Items per Row');
+        application_action.selector = ".owl-carousel";
+        application_action.attribute = 'data-rows';
+        application_action.action = true;
+        application_action.not_main_type = true;
+       // application_action.helplink = '1.2.29/docs/api/ng/directive/ngApp';
+        application_action.sections = {
+            'owl.app.parameters' : {
+                'name' : 'style',
+                'fields' : {
+                    'owl.app.text': {
+                        'type' : 'text',
+                        'name' : 'Type a Number:',
+                        'action' : 'element_attribute',
+                        'attribute' : 'data-rows',
+                        'attribute_keep_if_empty' : false
+                    }
+                }
+            }
+        };
+         directives_actions.push(application_action);
+        application_action = new PgComponentType('owl-app-navigation', 'Enable/Disable Navigation');
+        application_action.selector = ".owl-carousel";
+        application_action.attribute = 'data-navigation';
+        application_action.action = true;
+        application_action.not_main_type = true;
+        //application_action.helplink = '1.2.29/docs/api/ng/directive/ngApp';
+        application_action.sections = {
+            'owl.app.parameters' : {
+                'name' : 'navigation',
+                'fields' : {
+                    'owl.app.text': {
+                         type: 'select',
+                            name: 'Navigation ?',
+                            action: 'element_attribute',
+                            attribute : 'data-navigation',
+                            'show_empty' : false,
+                            'options' : [
+                                {'key' : 'true', 'name' : 'Yes'},
+                                
+                            ]
+                    }
+                }
+            }
+        };
+        directives_actions.push(application_action);
+                application_action = new PgComponentType('owl-app-single', 'Enable Single Item');
+        application_action.selector = ".owl-carousel";
+        application_action.attribute = 'data-single';
+        application_action.action = true;
+        application_action.not_main_type = true;
+        //application_action.helplink = '1.2.29/docs/api/ng/directive/ngApp';
+        application_action.sections = {
+            'owl.app.parameters' : {
+                'name' : 'singleItem',
+                'fields' : {
+                    'owl.app.text': {
+                         type: 'select',
+                            name: 'Single Item ?',
+                            action: 'element_attribute',
+                            attribute : 'data-single',
+                            'show_empty' : false,
+                            'options' : [
+                                {'key' : 'true', 'name' : 'Yes'},
+                                
+                            ]
+                    }
+                }
+            }
+        };
+        directives_actions.push(application_action);
+
+        var addComponentTypesToPG = function(list) {
+            for(var i = 0; i < list.length; i++) {
+                f.addComponentType(list[i]);
+            }
+        }
+
+        addComponentTypesToPG(directives_actions);
+         var section = new PgFrameworkLibSection('owldirectivesactions', 'Owl Carosel');
+        section.setComponentTypes( directives_actions );
+        section.closed = true;
+        f.addActionsSection(section);
         //Now, lets define sections and elements shown in LIB tab
         var section = new PgFrameworkLibSection('owl-elements', 'Owl Slider');
         //Pass components in array
